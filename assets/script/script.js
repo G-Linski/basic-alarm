8c7596fa0c9acfcf5855b1be3434d182dbf54c9b
Gabe <G_Linski@hotmail.com>
2024-03-18 21:46:44 -0500

    added delay

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,21 @@
 
 
 const clock = document.querySelector('.clock');
+const body = document.querySelector('body');
 //Alarm
 const alarmSound = new Audio('./assets/audio/alarm.mp3');
 alarmSound.type = 'audio/mp3';
 const alarmButton = document.querySelector('.alarm-button');
-// const alarm = document.querySelector('.alarm-box p');
 const alarm = document.querySelector('.alarm');
 const hoursInput = document.getElementById('hours-input');
 const minutesInput = document.getElementById('minutes-input');
-const body = document.querySelector('body');
 const bell = document.querySelector('.bell');
-
 const alarmTime = new Date(0,0,0);
+
 let alarmSet = false;
 let wakeHour = -1;
 let wakeMinute = -1;
-
-
+let delay = false;
 
 //Clock Display Functions
 function formatTime(hours, minutes){
@@ -45,21 +43,40 @@ function p(x){
   return parseInt(x);
 }
 
+//Delay if Same Time
+function delayReset(){
+  delay = false;
+}
+
+//Display on Clock and Alarm
 function displayTime() {
   const now = new Date();
   let hours = now.getHours();
   let minutes = now.getMinutes();
   hours,minutes = formatTime(hours,minutes);
   clock.innerText =  hours + ":" + minutes;
-
-
-
-  if (p(hours) === p(wakeHour) && p(minutes) === p(wakeMinute) && alarmSet){
+  
+  console.log(delay);
+  if(sameTime(hours,wakeHour,minutes,wakeMinute) && alarmSet && !delay){
     alarmSound.play();
     setTimeout(resetAlarm, 8000);
+    delayReset();
 
   }
 }
+
+
+//Are Times Equal
+function sameTime(h1,h2,m1,m2){
+  if (p(h1) === p(h2)){
+    if(p(m1)===p(m2)){
+      return true;
+    }
+  }
+  return false;
+  }
+
+
 
 
 //Alarm Functions
@@ -81,15 +98,27 @@ function validateTime(hours,minutes){
   return (check1 && check2);
 }
 
-
 alarmButton.addEventListener('click',function(){
   wakeHour = hoursInput.value;
   wakeMinute = minutesInput.value;
   if(validateTime(wakeHour,wakeMinute)){
+
     wakeHour,wakeMinute = formatTime(wakeHour,wakeMinute);
     alarm.innerHTML = wakeHour + ":" + wakeMinute;
     alarmSet = true;
     bell.style.visibility = "visible";
+
+    const currentTime = new Date();
+    let h1 = currentTime.getHours();
+    let m1 = currentTime.getMinutes();
+    if(sameTime(h1,wakeHour,m1,wakeMinute)){
+      delay = true;
+      setTimeout(delayReset, 60000);
+    }
+    
+
+
+
   }
 
   else {
@@ -100,14 +129,6 @@ alarmButton.addEventListener('click',function(){
   
 });
 
-
-
-// alarmButton.addEventListener('click',function(){
-//   let wakeHour = hoursInput.value;
-//   let wakeMinute = minutesInput.value;
-//   clock.innerText = wakeHour;
-//   console.log(wakeHour);
-// })
 
 
 
